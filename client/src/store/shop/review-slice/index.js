e1fe5de22670226a82356fb81d3ad1173f6e7d18
rishimalviya <malviyaabhi059@gmail.com>
2025-06-28 10:30:24 +0530

    Complete E-commerce project

---
+++ client/src/store/shop/review-slice/index.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  reviews: [],
+};
+
+export const addReview = createAsyncThunk(
+  "/order/addReview",
+  async (formdata) => {
+    const response = await axios.post(
+      `${API_BASE_URL}/shop/review/add`,
+      formdata
+    );
+
+    return response.data;
+  }
+);
+
+export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
+  const response = await axios.get(
+    `${API_BASE_URL}/shop/review/${id}`
+  );
+
+  return response.data;
+});
+
+const reviewSlice = createSlice({
+  name: "reviewSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getReviews.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getReviews.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.reviews = action.payload.data;
+      })
+      .addCase(getReviews.rejected, (state) => {
+        state.isLoading = false;
+        state.reviews = [];
+      });
+  },
+});
+
+export default reviewSlice.reducer;
